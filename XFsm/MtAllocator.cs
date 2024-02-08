5ebf0448fc8594115037541999950dd1c385e9f6
Fexty <bogofogo1@gmail.com>
2024-02-08 16:28:06 +0100

    XFsm project

---
+++ XFsm/MtAllocator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpPluginLoader.Core;
+
+namespace XFsm;
+internal class MtAllocator : MtObject
+{
+    public MtAllocator(nint instance) : base(instance) { }
+    public MtAllocator() : base() { }
+
+    public unsafe void Free(nint ptr)
+    {
+        new NativeAction<nint, nint>(GetVirtualFunction(13)).Invoke(Instance, ptr);
+    }
+}
